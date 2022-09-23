5975516584e6108d7936795082076572db0002bc
Collier23 <ryancollier1994@gmail.com>
2022-09-23 16:32:25 +1000

    wasnt happy with the way java was heading so decided to start again

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,29 @@
-// input storage from the user
-// pull the id from input group
-let city = $("#searchTerm").val()
+// Todays date
+let currentDay = moment().format("L");
+// Day of the week
+let weekDay = moment().format("dddd");
 
-// const variable to store api key
-const apiKey = "&appid=419a6f585c25a4bb328b301f9ff349f4";
-// date variable using Date to turn date/time into a string.
-let date = new Date();
+// populate city search history
+var cities = JSON.parse(localStorage.getItem("cities")) || [];
+var cityList = $("#city-list");
+var value = localStorage.getItem("cities");
+var pvalue = JSON.parse(value);
 
-$("#searchTerm").keypress(function(event) {
-    // key code 13 is enter
-    if (event.keycode === 13) {
-        event.preventDefault();
-        $('#searchTerm').click();
-    }
-});
 
-$("#searchTerm").on("click", function() {
-    //use css to show the forcast header
-    $('#forcastH5').addClass('');
-
-    // get value from the input tag store in city
-    city = $("#searchTerm").val();
-
-    //clear the input box
-    $("#searchTerm").val("");
-
-    //api url with city input and api key
-    
-    const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-
-    //ajax call
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-
-        console.log(response)
-        console.log(response.name)
-        console.log(response.main.humidity)
-        console.log(response.wind.speed)
-        console.log(response.weather[0].icon)
-
-        getCurrentConditions(response);
-        getCurrentForecast(response);
-
-        // need a list
-        generateList();
-    })
-});
-
-function generateList() {
-    let listItems = $("<li>").addClass("list-group-item").text(city);
-    //append items to the list
-    $(".list").append(listItems);
+if(value == null) {
+    $(".forecastHeader").hide(); 
+    // hides 5 day forecast HTML
 }
+
+if(pvalue != null) {
+    for(var i=0; i <pvalue.length;i++) {
+        cityList.prepend('<p class="city-values'> + pvalue[i] + '</p>');
+
+        $(".city-values").hover(function() {
+            $(this).css("background-color", "lightgray");
+        }, function() {
+            $(this).css("background-color", "white");
+        });
+    }
+}
+
