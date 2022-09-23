c15c6450704e9544d5f9ea173dfd0b7fd4bde00d
Collier23 <ryancollier1994@gmail.com>
2022-09-23 16:04:46 +1000

    added ajax call and generateList function

--- assets/script.js
+++ assets/script.js
@@ -28,3 +28,29 @@ $("#searchTerm").on("click", function() {
     //api url with city input and api key
     
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+    //ajax call
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response)
+        console.log(response.name)
+        console.log(response.main.humidity)
+        console.log(response.wind.speed)
+        console.log(response.weather[0].icon)
+
+        getCurrentConditions(response);
+        getCurrentForecast(response);
+
+        // need a list
+        generateList();
+    })
+});
+
+function generateList() {
+    let listItems = $("<li>").addClass("list-group-item").text(city);
+    //append items to the list
+    $(".list").append(listItems);
+}
